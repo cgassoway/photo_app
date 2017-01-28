1c5a8af3e74b3eb7513b0af17dacf1910478329e
Bill Gassoway <fbcwg123@gmail.com>
2017-01-28 01:42:28 +0000

    Implemented stripe purchase by credit card

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,10 @@
 Rails.configuration.stripe = {
   :publishable_key => ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
   :secret_key => ENV['STRIPE_TEST_SECRET_KEY']
+  
+  #:publishable_key => 'pk_test_UifrCxR7UvkgeulA7nUWcQ0L',
+  #:secret_key => 'sk_test_xl9chczjSFvGmFbWj575kiCs'
+
 }
 
-Stripe.api_key = Rails.configuration_stripe[:secret_key]
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
